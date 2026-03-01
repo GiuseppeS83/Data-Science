180cab1072406f46478b0d6d6edc4d2bb5918571
Giuseppe <136174578+GiuseppeS83@users.noreply.github.com>
2026-03-01 19:44:53 +0100

    Add files via upload

--- calcola_media/calcola_media.py
+++ calcola_media/calcola_media.py
@@ -1,1 +1,20 @@
+def calcola_media(lista_numeri):
+    """
+    Calcola la media di una lista di numeri.
+    
+    Parametri:
+    lista_numeri (list): Lista di numeri (interi o float)
+    
+    Ritorna:
+    float: La media dei numeri, oppure None se la lista è vuota
+    """
+    if not lista_numeri:  # Controlla se la lista è vuota
+        return None
+    somma = sum(lista_numeri)
+    quantita = len(lista_numeri)
+    media = somma / quantita
+    return media
 
+# Esempio di utilizzo
+numeri = [10, 20, 30, 40, 50]
+print("La media è:", calcola_media(numeri))
