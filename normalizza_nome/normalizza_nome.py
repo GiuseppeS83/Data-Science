3f340469ad8fa474c04c08657f1018aeb6d0c4a6
Giuseppe <136174578+GiuseppeS83@users.noreply.github.com>
2026-03-01 19:23:52 +0100

    Add files via upload

--- normalizza_nome/normalizza_nome.py
+++ normalizza_nome/normalizza_nome.py
@@ -1,1 +1,6 @@
-
+def normalizza_nome(nome):
+    nome = nome.strip()
+    return nome
+print(normalizza_nome("  marco rossi  "))      # Output: "Marco Rossi"
+print(normalizza_nome("giovanni de LUCA"))    # Output: "Giovanni De Luca"
+print(normalizza_nome("  anna-maria lombardi")) # Output: "Anna-Maria Lombardi"
